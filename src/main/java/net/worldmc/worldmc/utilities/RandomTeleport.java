88fd88ea49d9c26f2579d7d4e1563d44fd32e047
Zeke Parry <pariezekiel@gmail.com>
2022-04-04 16:03:09 +1000

    y to z correction

--- src/main/java/net/worldmc/worldmc/utilities/RandomTeleport.java
+++ src/main/java/net/worldmc/worldmc/utilities/RandomTeleport.java
@@ -27,7 +27,7 @@ public class RandomTeleport {
                 }
                 attempts ++;
                 int x = new Random().nextInt(Worldmc.getInstance().getConfig().getInt("RandomTeleport.X")) * (new Random().nextBoolean() ? -1 : 1);
-                int z = new Random().nextInt(Worldmc.getInstance().getConfig().getInt("RandomTeleport.Y")) * (new Random().nextBoolean() ? -1 : 1);
+                int z = new Random().nextInt(Worldmc.getInstance().getConfig().getInt("RandomTeleport.Z")) * (new Random().nextBoolean() ? -1 : 1);
 
                 world.getChunkAtAsync(new Location(world, x, 0, z)).thenAccept(chunk -> {
                     Block highestBlock = world.getHighestBlockAt(x, z);
