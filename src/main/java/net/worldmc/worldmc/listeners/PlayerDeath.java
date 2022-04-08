07a23e572803ddb6c106817281c46e4b2cd730a8
Zeke Parry <pariezekiel@gmail.com>
2022-04-08 21:00:40 +1000

    check for town spawn on death

--- src/main/java/net/worldmc/worldmc/listeners/PlayerDeath.java
+++ src/main/java/net/worldmc/worldmc/listeners/PlayerDeath.java
@@ -1,5 +1,6 @@
 package net.worldmc.worldmc.listeners;
 
+import com.palmergames.bukkit.towny.TownyAPI;
 import net.worldmc.worldmc.utilities.RandomTeleport;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,7 +13,7 @@ public class PlayerDeath implements Listener {
     public void playerDeathEvent(PlayerDeathEvent event) {
         Player player = event.getPlayer();
 
-        if (player.getBedSpawnLocation() != null) {
+        if (player.getBedSpawnLocation() != null || TownyAPI.getInstance().getTownSpawnLocation(player) != null) {
             return;
         }
 
